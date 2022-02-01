138e2cd0909ed9e30a6fc372a16d9137159d632a
aaronkg1 <aaronkg1@gmail.com>
2022-02-01 15:59:43 +0000

    Basic functionality added to calculator. Known problem with cancel button not working correctly

--- calculator.js
+++ calculator.js
@@ -5,29 +5,93 @@ const clearButton = document.querySelector("#clear");
 const addButton = document.querySelector("#add");
 const divideButton = document.querySelector("#divide")
 const subtractButton = document.querySelector("#subtract");
-const multiplyButton = document.querySelector("multiply");
-const equalsButton = document.querySelector("equals");
-let displayValue;
-let numberToAdd;
+const multiplyButton = document.querySelector("#multiply");
+const equalsButton = document.querySelector("#equals");
+const cancelButton = document.querySelector("#cancel")
+let valueOne = 0;
+let valueTwo = 0;
+let operateValue = "";
+
 
 numbers.forEach(number => number.addEventListener('click', () => {
+    if (operateValue === "") {
+    valueOne += number.value;
     output.textContent += number.value;
-    displayValue = output.textContent;
+}
+
+else {
+    valueTwo += number.value;
+    output.textContent += number.value;
+
+}
+
+    if (output.textContent.length > 11) {
+        output.textContent = output.textContent.slice(0,11);
+    }
 }));
+
+
 
 clearButton.addEventListener('click', () => {
     
+    output.textContent = " ";
+    valueOne = "";
+    valueTwo = "";
+    operateValue = "";
+});
+
+cancelButton.addEventListener('click', (e) => {
+    output.textContent = output.textContent.slice(0, output.textContent.length -1);
+
+    if (operateValue = "") {
+        valueOne = output.textContent;
+    }
+
+    else {
+        valueTwo = output.textContent;
+    }
 })
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', (event) => {
     
+    output.textContent = '';
+    operateValue = "add";  
+    
+});
+
+divideButton.addEventListener('click', () => {
+    output.textContent = '';
+    operateValue = "divide";  
+});
+
+subtractButton.addEventListener('click', () => {
+    output.textContent = '';
+    operateValue = "subtract";  
+});
+
+multiplyButton.addEventListener('click', () => {
+    output.textContent = '';
+    operateValue = "multiply";  
+});
+
+equalsButton.addEventListener('click', () => {
+    let toNumberOne = parseInt(valueOne);
+    let toNumberTwo = parseInt(valueTwo);
+    valueTwo = "0";
+    
+    result = operate(operateValue, toNumberOne, toNumberTwo);
+    output.textContent = result;
+    valueOne = result;
+    
+
+
 })
-
-
 
 
 function add(x, y) {
-    return x + y;
+    let result = x + y;
+    console.log(result);
+    return result;
 }
 
 function subtract(x, y) {
@@ -43,13 +107,13 @@ function divide(x, y) {
 }
 
 function operate(operator, x, y) {
-    if (operator == "+") {
+    if (operator == "add") {
         return add(x, y);
-    } else if (operator == "-") {
+    } else if (operator == "subtract") {
         return subtract(x, y);
-    } else if (operator == "*") {
+    } else if (operator == "multiply") {
         return multiply(x, y)
-    } else if (operator == "/") {
+    } else if (operator == "divide") {
        return divide(x, y);
     }
 }
